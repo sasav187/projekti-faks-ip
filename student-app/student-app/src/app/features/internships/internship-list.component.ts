62437e091b63904a12f76c1dda9e49f5b7544b94
sasav187 <sasavujancevic@gmail.com>
2026-03-01 18:19:23 +0100

    angular internships fix

--- student-app/student-app/src/app/features/internships/internship-list.component.ts
+++ student-app/student-app/src/app/features/internships/internship-list.component.ts
@@ -2,33 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InternshipService } from '../../core/services/internship.service';
 import { Internship } from '../../shared/models/internship.model';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'app-internship-list',
   imports: [CommonModule],
-  templateUrl: './internship-list.html',
-  styleUrls: ['./internship-list.css']
+  templateUrl: './internship-list.component.html',
+  styleUrls: ['./internship-list.component.css']
 })
 export class InternshipListComponent implements OnInit {
 
-  internships: Internship[] = [];
+  internships$!: Observable<Internship[]>;
   loading = true;
   errorMessage = '';
 
   constructor(private internshipService: InternshipService) {}
 
   ngOnInit(): void {
-    this.internshipService.getAll().subscribe({
-      next: (data: Internship[]) => {
-        this.internships = data;
-        this.loading = false;
-      },
-      error: (err) => {
-        console.error(err);
+    this.internships$ = this.internshipService.getAll().pipe(
+      tap(() => this.loading = false),
+      catchError(err => {
+        console.error('Error loading internships:', err);
         this.errorMessage = 'Failed to load internships.';
         this.loading = false;
-      }
-    });
+        return of([]);
+      })
+    );
   }
 }
